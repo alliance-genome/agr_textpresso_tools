b75279c240366f1d4fd9e75949e69a5c50bda8b6
Valerio Arnaboldi <valerio.arnaboldi@gmail.com>
2023-09-08 10:12:38 -0700

    Remove redundant build-cloud.sh script, update run-cloud.sh
    The 'build-cloud.sh' script was deleted as it contained redundant rsync commands which were causing unnecessary duplication. In the 'run-cloud.sh' script, symbolic link called 'tokenize' was updated to point to '03pdf2cas.sh' from '03pdf2cas4tai.sh' to streamline document processing flow.

--- build-cloud.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-rsync -a /home/mueller/work/swd/textpressocentral .
-rsync -a /home/mueller/work/swd/tpctools .
-rsync -a /home/mueller/work/swd/textpressoapi .
-docker build --no-cache -f Dockerfile-cloud -t $1 .
-rm -rf textpressocentral tpctools textpressoapi
