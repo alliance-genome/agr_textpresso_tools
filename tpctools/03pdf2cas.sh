eb5e81b16fe54f9839416a304a993eb2de9f1535
Valerio Arnaboldi <valerio.arnaboldi@gmail.com>
2023-09-09 16:46:19 -0700

    Restart pdf2cas if it gets killed
    Restart processing pdfs and convert them to cas files in case articles2cas gets killed. Continue processing only the remaining files and terminate when the number of dirs in the cas folder matches the number of pdf dirs

--- tpctools/03pdf2cas.sh
+++ tpctools/03pdf2cas.sh
@@ -18,15 +18,15 @@ N_PROC=8
 LOCKFILE="/data/textpresso/tmp/03pdf2cas.lock"
 if [[ -f "${LOCKFILE}" ]]
 then
-    echo $(basename $0) "is already running."
-    exit 1
+  echo $(basename $0) "is already running."
+  exit 1
 else
-    touch "${LOCKFILE}"
-    while [[ $# -gt 0 ]]
-    do
-	key=$1
-	
-	case $key in
+  touch "${LOCKFILE}"
+  while [[ $# -gt 0 ]]
+  do
+    key=$1
+
+	  case $key in
 	    -p|--pdf-dir)
 		shift
 		PDF_DIR="$1"
@@ -49,53 +49,60 @@ else
 		echo "wrong argument: $key"
 		echo
 		usage
-	esac
-    done
+	  esac
+  done
     
-    export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/usr/local/lib
-    export PATH=$PATH:/usr/local/bin
+  export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/usr/local/lib
+  export PATH=$PATH:/usr/local/bin
     
-    echo "Generating CAS1 files ..."
+  echo "Generating CAS1 files ..."
     
-    cd ${PDF_DIR}
-    # obtain all the folder names in PDF_DIR then create tpcas1 folders for every corpus
-    for folder in */ ;
-    do
-	mkdir -p "${CAS1_DIR}/${folder}"
-    done
+  cd ${PDF_DIR}
+  # obtain all the folder names in PDF_DIR then create tpcas1 folders for every corpus
+  for folder in */ ;
+  do
+	  mkdir -p "${CAS1_DIR}/${folder}"
+  done
     
-    # check for newer files in each pdf folder, make processing list
-    for folder in *;
-    do
-	echo "${folder}"
-	counter=0
-	rm -f /tmp/"${folder}".*.list
-	for i in "${folder}"/*
-	do
+  # check for newer files in each pdf folder, make processing list
+  for folder in *;
+  do
+	  echo "${folder}"
+	  rm -f /tmp/"${folder}".*.list
+    rm -f /tmp/"${folder}".*.processed
+	  counter=0
+	  for i in "${folder}"/*
+	  do
 	    if [[ $i -nt "${CAS1_DIR}/$i" ]]
 	    then
-		d=${i#"${folder}"/}
-		bin=$(($counter % $N_PROC))
-		echo $d >> "/tmp/${folder}.$bin.list"
-		counter=$(($counter+1))
+		    d=${i#"${folder}"/}
+		    bin=$(($counter % $N_PROC))
+		    echo $d >> "/tmp/${folder}.$bin.list"
+		    touch ""/tmp/"${folder}.$bin.processed"
+		    counter=$(($counter+1))
 	    fi
-	done
-    done
-    # run article2cas, with CAS1_DIR as CWD
-    cd ${CAS1_DIR}
-    for folder in *
-    do
-	for ((j=0; j<${N_PROC}; j++))
-	do
-	    if [[ -f "/tmp/${folder}.$j.list" ]]
-	    then
-		articles2cas -i "${PDF_DIR}/${folder}" -l "/tmp/${folder}.$j.list" -t 1 -o "${folder}" -p &
-	    fi
-	done
-	wait
-	# gzip all tpcas files
-	find "${folder}" -name "*tpcas" -print0 | xargs -0 -n 8 -P 8 pigz 2>/dev/null
-	rm -f /tmp/"${folder}".*.list
-    done
-    rm ${LOCKFILE}
+	  done
+  done
+  # run article2cas, with CAS1_DIR as CWD
+  cd ${CAS1_DIR}
+  for folder in *
+  do
+    while [[ $(ls -l "${folder}" | wc -l) -lt $(ls -l "${PDF_DIR}/${folder}" | wc -l) ]]
+	  do
+	    for ((j=0; j<${N_PROC}; j++))
+	    do
+	      grep -v -x -f <(sort "/tmp/${folder}.$j.processed" | uniq) "/tmp/${folder}.$j.list" > "/tmp/${folder}.$j.list.tmp"
+        mv "/tmp/${folder}.$j.list.tmp" "/tmp/${folder}.$j.list"
+	      if [[ $(wc -l "/tmp/${folder}.$j.list") -gt 0 ]]
+	      then
+	        articles2cas -i "${PDF_DIR}/${folder}" -l "/tmp/${folder}.$j.list" -t 1 -o "${folder}" -p | grep -o -e "AGRKB:[0-9]*" > "/tmp/${folder}.$j.processed" &
+	      fi
+	    done
+	    wait
+	  done
+	  # gzip all tpcas files
+	  find "${folder}" -name "*tpcas" -print0 | xargs -0 -n 8 -P 8 pigz 2>/dev/null
+	  rm -f /tmp/"${folder}".*.list
+  done
+  rm ${LOCKFILE}
 fi
