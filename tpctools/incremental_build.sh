b136133548a8d70ca33b209f1c26896c05bf5287
sweng66 <sweng@stanford.edu>
2023-10-02 08:19:11 -0700

    adding checking for cas1 and cas2 files generation + empty file checking, also getting rid of check_data party since it doesn't work in cron, also doesn't add much to data checking

--- tpctools/incremental_build.sh
+++ tpctools/incremental_build.sh
@@ -3,41 +3,56 @@
 raw_file_dir="/data/textpresso/raw_files_new"
 cas1_dir="/data/textpresso/tpcas-1_new"
 cas2_dir="/data/textpresso/tpcas-2_new"
+index_dir="/data/textpresso/luceneindex_new"
 
-rm -rf ${raw_file_dir}
+rm -rf "${raw_file_dir}"
 rm -rf "${cas1_dir}"
 rm -rf "${cas2_dir}"
+rm -rf "${index_dir}"
 
-conda run -n agr_textpresso python3 /data/textpresso/tpctools/getPdfBiblio/download_pdfs_bib_files.py -m "${MOD}" -d 7 -p "${raw_file_dir}"
+## downloading pdfs and generating bib files
+
+conda run -n agr_textpresso python3 /data/textpresso/tpctools/getPdfBiblio/download_pdfs_bib_files.py -m "${MOD}" -d 6 -p "${raw_file_dir}"
 
 echo "DONE downloading PDFs and generating bib files!"
 
-check_data.sh -i
+echo -n "Total new PDF file(s): "
+find "${raw_file_dir}/pdf" -maxdepth 3 -name "*.pdf" | wc -l
 
-echo "DONE running check data for ontology files!"
+## generating CAS-1 files
 
 tokenize -P 4 -p "${raw_file_dir}/pdf" -c ${cas1_dir}
 
-echo "DONE generating cas-1 files!"
+echo "DONE generating cas-1 files!" 
 
-check_data.sh -1
+echo -n "Total new CAS-1 file(s): "
+find "${cas1_dir}" -maxdepth 3 -name "*.tpcas*" | wc -l
 
-echo "DONE running check data for cas1 files!"
+echo -n "Empty CAS-1 file(s): "
+find "${cas1_dir}"  -maxdepth 3 -type f -name "*.tpcas.gz" -exec ls -l {} \; | grep " 0 " | wc -l
+
+## generating CAS-2 files
 
 annotate -P 2 -c ${cas1_dir} -C ${cas2_dir}
 
 echo "DONE cas-2 files generation!"
 
-check_data.sh -2
+echo -n "Total new CAS-2 file(s): "                                                                                                                               
+find "${cas2_dir}" -maxdepth 3 -name "*.tpcas*" | wc -l 
 
-echo "DONE running check data for cas2 files!"
-    
+echo -n "Empty CAS-2 file(s): "                                                                                                                                    
+find "${cas2_dir}"  -maxdepth 3 -type f -name "*.tpcas.gz" -exec ls -l {} \; | grep " 48 " | wc -l
+
+## syncing bib files
+     
 rsync -av "${raw_file_dir}/bib/" "${cas2_dir}/"
 
 echo "DONE transferring bib files over to tpcas-2 folder!"
 
-# /data/textpresso/tpctools/incremental_index.sh -C ${cas2_dir}
+## indexing the new papers
 
-# echo "DONE indexing!"
+/data/textpresso/tpctools/incremental_index.sh -C ${cas2_dir}
+
+echo "DONE indexing!"
 
 conda run -n agr_textpresso python3 /data/textpresso/tpctools/send_report.py
