9e73fa5d5ac70e4b832c013f490d55e01a3918ca
sweng66 <sweng@stanford.edu>
2023-10-04 21:25:32 -0700

    adding reference curie list for the newly added papers to slack report

--- tpctools/send_report.py
+++ tpctools/send_report.py
@@ -23,6 +23,7 @@ def compose_message():
     total_indexed = None
     empty_cas1_files = None
     empty_cas1_files = None
+    new_papers = []
     for line in f:
         if line.startswith("Total new PDF file(s):"):
             total_pdf_count = line.strip().replace("Total new PDF file(s):", "")
@@ -33,7 +34,10 @@ def compose_message():
         if line.startswith("Total new CAS-2 file(s):"):
             total_cas2_count = line.strip().replace("Total new CAS-2 file(s):", "")
         if line.startswith("Empty CAS-2 file(s):"):
-            empty_cas2_files = line.strip().replace("Empty CAS-2 file(s):", "") 
+            empty_cas2_files = line.strip().replace("Empty CAS-2 file(s):", "")
+        if "tpcas-2_new" in line and 'AGRKB:' in line:
+            ref_curie = line.strip().split('/')[-1].split('.tpcas')[0]
+            new_papers.append(ref_curie) 
     f.close()
     
     f = open(indexCountFile)
@@ -50,6 +54,8 @@ def compose_message():
         rows = rows + f"<tr><th style='text-align:left' width='300'>{label}</th><td width='100'>{count}</td></tr>"
     email_message = "<table></tbody>" + rows + "</tbody></table>"
     email_message = email_message + "<p>The logfile is available at /tmp/incremental_build.log</p>"
+    email_message = email_message + "<p>The full-text files for the following papers have just been added to Textpresso."
+    email_message = email_message + "<p>" + "<br>".join(new_papers) + "<p>" 
     return email_message	
 
 
