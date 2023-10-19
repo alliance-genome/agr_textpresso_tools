d540a2e235a556ff6e274f41ae19bf94dca193ae
Valerio Arnaboldi <valerio.arnaboldi@gmail.com>
2023-10-19 14:53:45 -0700

    Replace old text conversion script
    Replaced old script '03pdf2cas.sh' with a new script '03pdf2cas4tai.sh' to cater for occasional script failure. This new script includes a feature to forcibly restart upon encountering an error, ensuring all text files are eventually processed successfully. A new conversion script 'tai.sh' has also been added for improved text conversion.

--- start_textpresso.sh
+++ start_textpresso.sh
@@ -3,7 +3,8 @@
 cron
 mkdir -p /data/textpresso/textpressoapi_data
 mkdir -p /data/textpresso/postgres
-ln -s /usr/local/bin/03pdf2cas.sh /usr/local/bin/tokenize
+ln -s /usr/local/bin/tai.sh /usr/local/bin/convert_text
+ln -s /usr/local/bin/03pdf2cas4tai.sh /usr/local/bin/tokenize
 ln -s /usr/local/bin/07cas1tocas2.sh /usr/local/bin/annotate
 ln -s /usr/local/bin/12index.sh /usr/local/bin/index
 /root/initialize.sh -p
